67f8373417ae8bfbc10e811ce9b9f84326efe35d
arafatGithubCode <md489288@gmail.com>
2023-12-12 07:19:31 +0600

    object props passing

--- tutorial/src/components/User.tsx
+++ tutorial/src/components/User.tsx
@@ -1,17 +1,25 @@
 type UserProps = {
-  name: string;
-  age: number;
-  email: string;
-  isRegistered: boolean;
+  user: {
+    name: string;
+    age: number;
+    email: string;
+    isRegistered: boolean;
+    lang: string[];
+  };
 };
 
-const User = ({ name, age, email, isRegistered }: UserProps) => {
+const User = ({ user }: UserProps) => {
+  const { name, age, email, isRegistered, lang } = user;
   return (
     <div>
       <h2>{name}</h2>
       <p>Age: {age} years old</p>
       <p>{email}</p>
       {isRegistered ? <p>User is registered</p> : <p>User is not Registered</p>}
+
+      {lang.map((language, index) => (
+        <span key={index}>{language} </span>
+      ))}
     </div>
   );
 };
